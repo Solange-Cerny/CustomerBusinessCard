b3dc2e57e831ee91448a62b64d514458f87bffbb
Solange <sol.cer.87@gmail.com>
2021-10-09 11:43:49 +0100

    Add project files.

---
+++ CustomerBusinessCard/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerBusinessCard.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string HouseNo { get; set; }
+        public string StreetName { get; set; }
+        public string CityName { get; set; }
+        public string PostCode { get; set; }
+        public string Tel { get; set; }
+    }
+}
---
+++ CustomerBusinessCard/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerBusinessCard.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
